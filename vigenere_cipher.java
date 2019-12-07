c7acdcdd5de1f64323be33410bb14a5f640fcc69
AzimPathan <azimpathan737@gmail.com>
2019-12-07 19:26:21 +0530

    Corrected vigenere cipher for message with white spaces

--- vigenere_cipher.java
+++ vigenere_cipher.java
@@ -7,7 +7,13 @@ class vigenere_cipher
 		for (int i = 0, j = 0; i < Message.length(); i++) 
 		{
 			char ch = Message.charAt(i);
-			emsg += (char)(((ch - 65) + (Key.charAt(j)-65)) % 26 + 65);
+			
+			//emsg += (char)(((ch - 65) + (Key.charAt(j)-65)) % 26 + 65);
+			
+			if (ch < 'A' || ch > 'Z')
+                continue;
+            emsg += (char) ((ch + Key.charAt(j) - 2 * 'A') % 26 + 'A');
+
 			j = ++j % Key.length();
 		}
 		return emsg;
@@ -19,7 +25,12 @@ class vigenere_cipher
 		for (int i = 0, j = 0; i < Message.length(); i++)
 		{
 			char ch = Message.charAt(i);
-			dmsg += (char)((ch - Key.charAt(j) + 26) % 26 + 65);
+			//dmsg += (char)((ch - Key.charAt(j) + 26) % 26 + 65);
+			
+			if (ch < 'A' || ch > 'Z')
+                continue;
+            dmsg += (char) ((ch - Key.charAt(j) + 26) % 26 + 'A');
+
 			j = ++j % Key.length();
 		}
 		return dmsg;
@@ -36,9 +47,9 @@ class vigenere_cipher
 			{
 				System.out.print("Enter the key in UPPER Case: ");
 				String key = in.next();
-				in.
+				//in.skip('\n');
 				System.out.print("Enter message to encrypt: ");
-				String emsg = in.nextLine();
+				String emsg = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch!";//;in.next();
 				String encryptedMessage = encrypt(emsg, key);
 				System.out.println("Encrypted message : " + encryptedMessage);
 			}
@@ -46,9 +57,9 @@ class vigenere_cipher
 			{
 				System.out.print("Enter the key in UPPER Case: ");
 				String key = in.next();
-				in.skip('\n');
+				//in.skip('\n');
 				System.out.print("Enter message  to decrypt: ");
-				String dmsg = in.next();
+				String dmsg = "WMCEEIKLGRPIFVMEUGXQPWQVIOIAVEYXUEKFKBTALVXTGAFXYEVKPAGY";//in.next();
 				String decryptedMessage = decrypt(dmsg, key);
 				System.out.println("Decrypted message is: " + decryptedMessage);
 			}
